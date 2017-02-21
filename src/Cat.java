ec61fbe065a79e6e0948572b95171b3467116414
David Henningsen <henningsen@mymail.mines.edu>
2017-02-21 14:35:54 -0700

    Made all the cat faces look like lego faces and changed the meow printout to 'lego man'

--- src/Cat.java
+++ src/Cat.java
@@ -17,7 +17,7 @@ public class Cat {
 	private static final int MOUTH_HEIGHT = 10;
 	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
 	// mouth starts about 40% from left edge of head
-	private static final int MOUTH_X = HEAD_DIMENSION/5;
+	private static final int MOUTH_X = HEAD_DIMENSION/5*2;
 	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 3;
 	
 	// draw will render the Cat on the Graphics object
@@ -28,7 +28,11 @@ public class Cat {
 		int y=catY;
 		// Draw the head
 		g2.setColor(Color.yellow);
-		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
+		g2.fillRect(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
+		
+		g2.setColor(Color.yellow);
+		g2.fillRect(x+50, y-20, 60, 20);
+		
 		// Draw the eyes
 		g2.setColor(Color.blue);
 		x = catX + EYE_X; 
@@ -40,10 +44,10 @@ public class Cat {
 		g2.setColor(Color.black);
 		x = catX + MOUTH_X;
 		y = catY + MOUTH_Y;
-		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
+		g2.fillRect(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
 		g2.setColor(Color.black);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
-		g2.drawString("Meow", catX, catY+HEAD_DIMENSION+10);	
+		g2.drawString("Lego Man", catX, catY+HEAD_DIMENSION+10);	
 	}
 }
